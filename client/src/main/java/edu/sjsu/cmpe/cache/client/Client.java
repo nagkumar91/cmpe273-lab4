bb9a384768052b45f841d27d4789acf7cadf920b
Nagkumar <me@nagkumar.com>
2015-05-23 02:10:33 -0700

    Updated to satisfy requirements for 2nd part

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,4 +1,5 @@
 package edu.sjsu.cmpe.cache.client;
+import com.mashape.unirest.http.Unirest;
 
 public class Client {
 
@@ -6,12 +7,44 @@ public class Client {
         System.out.println("Starting Cache Client...");
         CacheServiceInterface cache = new DistributedCacheService( "http://localhost", new Integer(3000), new Integer(3));
 
-        System.out.println("put(2 => bar)");
-        cache.put(2, "bar");
+        System.out.println("Trying to PUT 'a' to key 1 on all servers");
+        cache.put(1, "a");
 
-        String value = cache.get(2);
+		System.out.println("Sleeping for 30 seconds");
+        Thread.sleep(30000);
+
+        System.out.println("Trying to PUT 'b' to key 1 on all servers");
+        cache.put(1, "b");
+        
+        System.out.println("Sleeping for 30 seconds");
+        Thread.sleep(30000);
+
+        System.out.println("Trying to GET data for key 1 from all servers");
+        String value = cache.get(1);
         System.out.println("get(2) => " + value);
         System.out.println("Exiting Cache Client...");
+
+
+        Unirest.shutdown();
+    }
+
+}
+
+        System.out.println("STEP 1: Sleeping for 30 seconds");
+        Thread.sleep(30000);
+
+        System.out.println("STEP 2: PUT 1 => 'b' on all servers");
+        cache.put(1, "b");
+        System.out.println("STEP 2: Sleeping for 30 seconds");
+        Thread.sleep(30000);
+
+        System.out.println("STEP 3: GET 1 from all servers");
+        String value = cache.get(1);
+        System.out.println("STEP 3: get(2) => " + value);
+        System.out.println("Exiting Cache Client...");
+
+
+        Unirest.shutdown();
     }
 
 }
--- client/src/main/java/edu/sjsu/cmpe/cache/client/DistributedCacheService.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/DistributedCacheService.java
@@ -1,5 +1,6 @@
 package edu.sjsu.cmpe.cache.client;
 
+import com.mashape.unirest.http.Headers;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -9,56 +10,137 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
+
+
 /**
  * Distributed cache service
  * 
  */
 public class DistributedCacheService implements CacheServiceInterface {
     private final String caching_server_URL;
-    private final String get_URL;
     private final Integer start_port;
     private final Integer number_of_service;
     private final int write_Q;
+    private final int read_Q;
     private AtomicInteger number_of_success;
-    private AtomicInteger number_of_write_attempts;
+    private AtomicInteger number_of_attempts;
+    private String[] get_values;
+    private AtomicInteger get_index;
+    private CountDownLatch response_waiter;
+
+
 
     public DistributedCacheService(String server_URL, Integer start_port, Integer number_of_service) {
-        this.write_Q = 2;
+        this.write_Q = this.read_Q = 2;
         this.number_of_success = new AtomicInteger(0);
-        this.number_of_write_attempts = new AtomicInteger(0);
+        this.number_of_attempts = new AtomicInteger(0);
         this.number_of_service = number_of_service;
         this.caching_server_URL = server_URL;
         this.start_port = start_port;
-        this.get_URL = this.caching_server_URL + ":" + this.start_port.toString();
-
+        this.get_values = new String[this.number_of_service];
+        this.get_index = new AtomicInteger(0);
+        this.response_waiter = new CountDownLatch(this.number_of_service);
     }
 
+    // naive method to return string with maximum occurrence
+    private String getread_QString() {
+
+        HashMap<String, Integer> cntMap = new HashMap<String, Integer>();
+        int maxCount = 1;
+        String  retVal = this.get_values[0];
+        for (int j = 0; j < this.get_index.get(); j++) {
+            if (cntMap.containsKey(this.get_values[j])) {
+                int count = cntMap.get(this.get_values[j]).intValue();
+                cntMap.put(this.get_values[j], new Integer(count + 1));
+                if (count + 1 > maxCount) {
+                    retVal = this.get_values[j];
+                    maxCount = count + 1;
+                }
+            } else {
+                cntMap.put(this.get_values[j], 1);
+            }
+        }
+        if (maxCount == this.number_of_service) {
+            
+            retVal = null;
+        } else if ((maxCount < this.read_Q) && (retVal != null)) {
+            System.out.println("read Quorum not met.");
+            
+            for (int j = 0; j < this.get_index.get(); j++) {
+            
+                this.get_values[j] = null;
+            }
+        }
+        return retVal;
+    }
     /**
      * @see edu.sjsu.cmpe.cache.client.CacheServiceInterface#get(long)
      */
     @Override
     public String get(long key) {
-        HttpResponse<JsonNode> response = null;
         String value = null;
-        for (int i = 0 ; i < this.number_of_service.intValue(); i++) {
+        int i = 0;
+        while (i < this.number_of_service.intValue()) {
             Integer port = this.start_port + new Integer(i);
 
             String cacheUrl = this.caching_server_URL + ":" + port.toString();
-            try {
-                response = Unirest.get(cacheUrl + "/cache/{key}")
-                        .header("accept", "application/json")
-                        .routeParam("key", Long.toString(key)).asJson();
-                if (response != null) {
-                    value = response.getBody().getObject().getString("value");
-                    break;
-                }
-            } catch (UnirestException e) {
-                System.err.println(e);
-                continue;
-            }
+            System.out.println("Getting from server " + cacheUrl);
+            Future<HttpResponse<JsonNode>> future = Unirest.get(cacheUrl + "/cache/{key}")
+                    .header("accept", "application/json")
+                    .routeParam("key", Long.toString(key)).asJsonAsync(new Callback<JsonNode>() {
+
+                        @Override
+                        public void completed(HttpResponse<JsonNode> httpResponse) {
+                            synchronized (DistributedCacheService.this) {
+                                DistributedCacheService.this.number_of_attempts.getAndIncrement();
+                                if (httpResponse.getCode() != 200) {
+                                    System.out.println("Failed to get data from the cache. Have to perform a repair");
+                                } else {
+                                    
+                                    DistributedCacheService.this.number_of_success.getAndIncrement();
+                                    DistributedCacheService.this.get_values[DistributedCacheService.this.get_index.get()] =
+                                            httpResponse.getBody().getObject().getString("value");
+                                    //System.out.println(DistributedCacheService.this.get_index.get() + " => " + httpResponse.getBody().getObject().getString("value"));
+                                    DistributedCacheService.this.get_index.getAndIncrement();
+                                }
+                                response_waiter.countDown();
+                            }
+                        }
+
+                        @Override
+                        public void failed(UnirestException e) {
+                            DistributedCacheService.this.number_of_attempts.getAndIncrement();
+                            System.out.println("Failed to GET data. May need repair");
+                            response_waiter.countDown();
+                        }
+
+                        @Override
+                        public void cancelled() {
+                            DistributedCacheService.this.number_of_attempts.getAndIncrement();
+                            System.out.println("Cancelled");
+                            response_waiter.countDown();
+                        }
+                    });
+
+            i++;
+        }
+        try {
+            this.response_waiter.await();
+        } catch (Exception e) {
+            System.out.println("Error with exception " + e);
         }
 
-        return value;
+        String repairString = getread_QString();
+        if (repairString != null) {
+            System.out.println("String to repair with: " + repairString);
+            put(key, repairString);
+        } else {
+            repairString = this.get_values[0];
+        }
+        return repairString;
     }
 
     @Override
@@ -79,16 +161,16 @@ public class DistributedCacheService implements CacheServiceInterface {
                         @Override
                         public void completed(HttpResponse<JsonNode> httpResponse) {
                             if (httpResponse.getCode() != 204) {
-                                System.out.println("Failed to delete from the cache.");
+                                System.out.println("Failed to del from the cache.");
                             } else {
-                                System.out.println("Deleted the key: " + key);
+                                //System.out.println("Deleted " + key);
                                 DistributedCacheService.this.number_of_success.getAndIncrement();
                             }
                         }
 
                         @Override
                         public void failed(UnirestException e) {
-                            System.out.println("Failed with exception: " + e);
+                            System.out.println("Failed : " + e);
                         }
 
                         @Override
@@ -99,7 +181,7 @@ public class DistributedCacheService implements CacheServiceInterface {
 
         }
         DistributedCacheService.this.number_of_success.set(0);
-        DistributedCacheService.this.number_of_write_attempts.set(0);
+        DistributedCacheService.this.number_of_attempts.set(0);
     }
     /**
      * @see edu.sjsu.cmpe.cache.client.CacheServiceInterface#put(long,
@@ -121,16 +203,16 @@ public class DistributedCacheService implements CacheServiceInterface {
                     .routeParam("value", value).asJsonAsync(new Callback<JsonNode>() {
                         private void checkForSuccess() {
                             int flag = 0;
-                            System.out.println(DistributedCacheService.this.number_of_write_attempts.get() + " of " + DistributedCacheService.this.number_of_service);
-                            if (DistributedCacheService.this.number_of_write_attempts.get() == DistributedCacheService.this.number_of_service) {
+                            
+                            if (DistributedCacheService.this.number_of_attempts.get() == DistributedCacheService.this.number_of_service) {
                                 if (DistributedCacheService.this.number_of_success.get() >= DistributedCacheService.this.write_Q) {
-                                    System.out.println("Success in PUT!");
+                                    
                                 } else {
                                     flag = 1;
-                                    System.out.println("Failure in PUT. Rolling back " + DistributedCacheService.this.number_of_success.get() + " " + DistributedCacheService.this.write_Q);
+                                    
                                 }
 
-                                DistributedCacheService.this.number_of_write_attempts.set(0);
+                                DistributedCacheService.this.number_of_attempts.set(0);
                                 DistributedCacheService.this.number_of_success.set(0);
                                 if (flag == 1) {
                                     DistributedCacheService.this.delete(key);
@@ -140,27 +222,27 @@ public class DistributedCacheService implements CacheServiceInterface {
 
                         @Override
                         public void completed(HttpResponse<JsonNode> httpResponse) {
-                            DistributedCacheService.this.number_of_write_attempts.getAndIncrement();
-                            if (httpResponse.getCode() != 200) {
-                                System.out.println("Could not add to cache");
+                            synchronized (DistributedCacheService.this) {
+                                DistributedCacheService.this.number_of_attempts.getAndIncrement();
+                                if (httpResponse.getCode() != 200) {
+                                    System.out.println("Could not add to cache");
+                                } else {
+                                    DistributedCacheService.this.number_of_success.getAndIncrement();
+                                }
+                                checkForSuccess();
                             }
-                            else {
-                                System.out.println("key - " + key + " has value => " + value);
-                                DistributedCacheService.this.number_of_success.getAndIncrement();
-                            }
-                            checkForSuccess();
                         }
 
                         @Override
                         public void failed(UnirestException e) {
-                            DistributedCacheService.this.number_of_write_attempts.getAndIncrement();
-                            System.out.println("Failed with exception: " + e);
+                            DistributedCacheService.this.number_of_attempts.getAndIncrement();
+                            System.out.println("Failed to PUT data.");
                             checkForSuccess();
                         }
 
                         @Override
                         public void cancelled() {
-                            DistributedCacheService.this.number_of_write_attempts.getAndIncrement();
+                            DistributedCacheService.this.number_of_attempts.getAndIncrement();
                             System.out.println("Cancelled");
                             checkForSuccess();
                         }
